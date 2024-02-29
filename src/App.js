bbcfc1468f12d7dc6aebe0e3c7c18e6df748e780
spacecat <easyforlev@gmail.com>
2024-02-29 14:44:53 +0700

    Add fetch request for a  movie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
+import { useEffect } from "react";
+import SearchIcon from "./seacrh.svg";
 
-function App() {
-  return <h1>Hey</h1>;
+const API_URL = "https://www.omdbapi.com/?apikey=e89daccf";
+
+export default function App() {
+
+  async function seacrhMovies(title) {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+      return data.Search;
+  }
+
+
+  useEffect(() => {
+    seacrhMovies("Batman");
+  }, []);
+
+
+  return (
+    <div className="app">
+      <h1>Movie Finder</h1>
+
+      <div className="search">
+        <input placeholder="Search for movies" />
+      </div>
+    </div>
+  );
 }
 
-export default App;
+
